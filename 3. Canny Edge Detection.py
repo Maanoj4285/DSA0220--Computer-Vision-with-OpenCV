f3c7e4e4d22ee723ac59ec62188a3c7567071ce0
Maanoj4285 <160702383+Maanoj4285@users.noreply.github.com>
2024-11-12 12:57:25 +0530

    Output

--- 3. Canny Edge Detection.py
+++ 3. Canny Edge Detection.py
@@ -7,5 +7,6 @@ blurred_image = cv2.GaussianBlur(image, (5, 5), 0)
 edges = cv2.Canny(blurred_image, threshold1=100, threshold2=200)
 
 cv2.imshow("Canny Edge Detection", edges)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
