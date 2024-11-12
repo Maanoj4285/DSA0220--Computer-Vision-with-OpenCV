3ffa4545210344fbbafad20e2f5c64f63e135422
Maanoj4285 <160702383+Maanoj4285@users.noreply.github.com>
2024-11-12 13:00:33 +0530

    Output

--- 4. Image Dilation.py
+++ 4. Image Dilation.py
@@ -10,5 +10,6 @@ kernel = np.ones((1, 1), np.uint8)
 dilated_image = cv2.dilate(binary_image, kernel, iterations=20)
 
 cv2.imshow("Dilated Image", dilated_image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
