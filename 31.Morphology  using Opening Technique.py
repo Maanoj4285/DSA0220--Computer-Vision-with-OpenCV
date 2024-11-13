609419e3377ad3b20ef34b4ff8399be59ae995ac
Maanoj4285 <160702383+Maanoj4285@users.noreply.github.com>
2024-11-13 09:17:57 +0530

    Output

--- 31.Morphology  using Opening Technique.py
+++ 31.Morphology  using Opening Technique.py
@@ -10,7 +10,7 @@ kernel = np.ones((5, 5), np.uint8)
 opening = cv2.morphologyEx(binary_image, cv2.MORPH_OPEN, kernel)
 
 cv2.imshow("Original Image", image)
-cv2.imshow("Opening Technique Image", opening)
+cv2.imshow("Opening Technique", opening)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
