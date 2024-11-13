7a9799027d6f7e2b802e7b8d81236b4e1ef75280
Maanoj4285 <160702383+Maanoj4285@users.noreply.github.com>
2024-11-13 08:37:19 +0530

    Output

--- 21. Sharpening using Laplacian Neighbour.py
+++ 21. Sharpening using Laplacian Neighbour.py
@@ -1,15 +1,12 @@
 import cv2
 import numpy as np
 
-# Load image
 image = cv2.imread('data1.jpg', cv2.IMREAD_GRAYSCALE)
 
-# Define Laplacian mask with diagonal neighbors
 laplacian_extended_mask = np.array([[-1, -1, -1],
                                     [-1, 9, -1],
                                     [-1, -1, -1]])
 
-# Apply convolution using the Laplacian mask with diagonal neighbors
 sharpened_image = cv2.filter2D(image, -1, laplacian_extended_mask)
 
 cv2.imshow("Original Image", image)
