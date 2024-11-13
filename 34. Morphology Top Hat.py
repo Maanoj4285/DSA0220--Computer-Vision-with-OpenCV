98c198981c751c1b80c827afa48e6ea4cfc3bb3f
Maanoj4285 <160702383+Maanoj4285@users.noreply.github.com>
2024-11-13 09:30:51 +0530

    Output

--- 34. Morphology Top Hat.py
+++ 34. Morphology Top Hat.py
@@ -1,13 +1,10 @@
 import cv2
 import numpy as np
 
-# Load an image in grayscale mode
-image = cv2.imread('data1.jpg', 0)  # replace 'image_path' with the actual path to your image
+image = cv2.imread('data1.jpg', 0) 
 
-# Define the kernel for the morphological operation
-kernel = np.ones((15, 15), np.uint8)  # Larger kernel for more prominent features
+kernel = np.ones((15, 15), np.uint8)
 
-# Apply the Top Hat operation
 top_hat = cv2.morphologyEx(image, cv2.MORPH_TOPHAT, kernel)
 
 cv2.imshow("Top Hat Image",top_hat)
